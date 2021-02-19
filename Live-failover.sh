6f0e215c705f34e9c92bb96c2e6b6d10ccfd803a
uadm26 <77186949+uadm26@users.noreply.github.com>
2021-02-19 17:15:30 +0000

    added files
    Added files

---
+++ Live-failover.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+clear screen
+
+# Author: Joel Tricanji Version 1.000   Date: 28.03.2018
+
+echo "################## LIVE WEBSITE FAILOVER  #####################"
+echo
+
+# Variables to declare
+int_cfg_path="/etc/sysconfig/network-scripts"
+failover_file="FAILOVER-ifcfg-eth1"
+orig_file="ORIG-ifcfg-eth1"
+nic_card=eth1
+online_ip="136.153.26.16"
+
+# Beging of Script
+# Check for arguments
+
+	if [ -z "$1" ]
+		then
+
+	echo "		No argument has been supplied"
+	echo "		Example: sudo failover.sh offline "
+	echo
+echo "###############################################################"
+	exit
+	fi
+
+case $1 in
+    offline )
+		echo "		Bringging Live Server to Offline mode"
+		ifdown $nic_card > /dev/null 2>&1
+		sleep 5
+		cat $int_cfg_path/$failover_file > $int_cfg_path/ifcfg-$nic_card
+		sleep 5
+		if [ `ip a | grep $online_ip` ] 
+			then
+
+		echo " ### WARNING ### - SCRIPT ERROR FOLLOW MANUAL COMMAND - ifdown $nic_card"
+			else
+		
+		echo
+		echo " # INFO # - SCRIPT EXECUTED WITH SUCCESS - SERVER IS NOW OFFLINE"
+		fi
+		;;		
+    online )
+		echo "		Bringging Live Server to Online mode"
+
+		ifdown $nic_cardi > /dev/null 2>&1
+		sleep 5
+		cat $int_cfg_path/$orig_file > $int_cfg_path/ifcfg-$nic_card
+		sleep 5
+		ifup $nic_card > /dev/null 2>&1
+		sleep 10
+		if [ `ip a | grep $online_ip |wc -l` -ne "1" ]
+                        then
+
+                echo " ### WARNING ### - SCRIPT ERROR FOLLOW MANUAL COMMAND - ifup $nic_card"
+                        else
+
+                echo
+                echo " # INFO # - SCRIPT EXECUTED WITH SUCCESS - SERVER IS NOW ONLINE"
+                fi
+		;; 
+
+esac
+	
